7ab9aaaf01420835c969b62ddbff1d3fe380f0b6
atarajos <arnold.tarajos@smedix.com>
2016-07-22 15:28:53 +0300

    Test crucible, Jenkins and Test result analyzer plugin with a public repository

--- src/main/java/src/Application.java
+++ src/main/java/src/Application.java
@@ -1,10 +1,9 @@
 package src;
 
-public class Application
-{
+public class Application {
 
-  public static void main (String[] args)
-  {
-    System.out.println("This is the main function of the application");
-  }
+    public static void main(String[] args) {
+        System.out.println("We are testing jenkins and crucible and test result analyzer integration");
+        System.out.println("This is the main function of the application");
+    }
 }
---
+++ src/test/java/TestClassE.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class TestClassE {
+
+    @Test
+    public void testEqual() {
+        assertEquals(Boolean.TRUE, Boolean.TRUE);
+    }
+
+    @Test
+    public void testEmpty() {
+        assertNull(null);
+    }
+}
